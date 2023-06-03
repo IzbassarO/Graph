e8bff3b845c1863bf86d91ba0ab7ff519caadecb
Izbassar <izok2004@gmail.com>
2023-06-03 16:39:50 +0600

    MAIN class methods implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WeightedGraph<String> graph = new WeightedGraph<>(true);
+
+        graph.addEdge("CityA", "CityB", 2.1);
+        graph.addEdge("CityA", "CityC", 7.2);
+        graph.addEdge("CityC", "CityB", 3.9);
+        graph.addEdge("CityB", "CityD", 3.5);
+        graph.addEdge("CityC", "CityE", 5.4);
+
+        String source = "CityA";
+        String destination = "CityD";
+
+        BFS<String> bfs = new BFS<>(graph, source);
+        DFS<String> dfs = new DFS<>(graph, source);
+
+        System.out.println("Breadth-First Search:");
+        outputPath(bfs, destination);
+
+        System.out.println("\n\nDepth-First Search:");
+        outputPath(dfs, destination);
+    }
+
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
     }
 }
