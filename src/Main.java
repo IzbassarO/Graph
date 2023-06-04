17b00e9a454a9072b714d30cab1145909dc44f4b
Izbassar <izok2004@gmail.com>
2023-06-04 12:46:03 +0600

    returned back all commits to the initial one

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         WeightedGraph<String> graph = new WeightedGraph<>();
-        graph.addEdge("CityA", "CityB", 2.1);
-        graph.addEdge("CityA", "CityC", 7.2);
-        graph.addEdge("CityC", "CityB", 3.9);
-        graph.addEdge("CityB", "CityD", 3.5);
-        graph.addEdge("CityC", "CityE", 5.4);
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
 
         System.out.println("Dijkstra:");
-        Search<String> djk = new DijkstraSearch<>(graph, "CityA");
-        outputPath(djk, "CityD");
+        Search<String> djk = new DijkstraSearch<>(graph, "Almaty");
+        outputPath(djk, "Kyzylorda");
+
+        System.out.println("DFS:");
+        Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
+        outputPath(dfs, "Kyzylorda");
+
+        System.out.println("\n--------------------------------");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
     }
 
     public static void outputPath(Search<String> search, String key) {
